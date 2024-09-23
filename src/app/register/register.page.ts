a09495b4838706bbde469570d9c4e86b19115555
Ventzzz <vicenteherreraaranguiz89@gmail.com>
2024-09-23 00:19:27 -0300

    Se agrega inicio de sesion y registro funcional

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  formRegister: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private navCtrl: NavController
+  ) {
+    this.formRegister = this.fb.group({
+      Nombre: ['', [Validators.required, Validators.minLength(4)]],
+      contraseña: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  async registrar() {
+    if (this.formRegister.valid) {
+      const { Nombre, contraseña } = this.formRegister.value;
+      try {
+        await this.authService.register(Nombre, contraseña);
+        this.navCtrl.navigateRoot('/login'); 
+      } catch (err) {
+        if (err instanceof Error) {
+          console.error('Error al registrar usuario:', err.message);
+        } else {
+          console.error('Error desconocido:', err);
+        }
+      }
+    }
+  }
+  
 }
